8d6d33845d2f559a4d5233315f644b4261050901
HarishMahalingam0301 <harishmahalingam0301@gmail.com>
2024-01-04 22:39:10 +0530

    first

---
+++ src/Collection/ArrayList.java
@@ -0,0 +1,17 @@
+package Collection;
+import java.util.ArrayList;
+
+public class ArrayList {
+	
+	public static void main(String[] args) {
+		ArrayList xyz= new ArrayList();
+		xyz.add(10);
+		System.out.println(xyz);
+		
+	
+	
+	}
+	
+
+
+}
